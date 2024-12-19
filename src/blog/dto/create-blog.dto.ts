283708d8ce367ff61e9a50a48928670da68b1138
henzyd <henzydee@gmail.com>
2024-12-19 19:55:47 +0100

    added properties

--- src/blog/dto/create-blog.dto.ts
+++ src/blog/dto/create-blog.dto.ts
@@ -1,21 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, MinLength } from 'class-validator';
 
 export class CreateBlogDto {
+  @ApiProperty()
   @MinLength(3, {
     message: 'Title must be at least 3 characters',
   })
   title: string;
 
+  @ApiProperty()
   @MinLength(3, {
     message: 'Description must be at least 3 characters',
   })
   description: string;
 
+  @ApiProperty()
   @MinLength(3, {
     message: 'Subtitle must be at least 3 characters',
   })
   subtitle: string;
 
+  @ApiProperty()
   @IsOptional()
   image?: Express.Multer.File;
 }
